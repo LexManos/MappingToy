08675cda80a0e5635d93e8d88e6e06702d715488
coehlrich <coehlrich@users.noreply.github.com>
2022-06-17 03:22:42 +1200

    Allow for etags that aren't generated using AmazonS3 (#6)

--- src/main/java/net/minecraftforge/lex/mappingtoy/Utils.java
+++ src/main/java/net/minecraftforge/lex/mappingtoy/Utils.java
@@ -69,15 +69,15 @@ public class Utils {
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
 
-            if (!force && Files.isRegularFile(file))
-                connection.setRequestProperty("If-None-Match", '"' + HashFunction.MD5.hash(file) + '"');
-
+            Path etagFile = file.getParent().resolve(file.getFileName() + ".etag");
+            if (!force && Files.isRegularFile(etagFile) && Files.isRegularFile(file)) {
+                String foundEtag = Files.isRegularFile(etagFile) ? new String(readStreamFully(etagFile)) : "-";
+                connection.setRequestProperty("If-None-Match", '"' + foundEtag + '"');
+            }
             connection.connect();
 
             String etag = connection.getHeaderField("ETag");
-            if (etag == null)
-                etag = "-";
-            else if ((etag.startsWith("\"")) && (etag.endsWith("\"")))
+            if (etag != null && (etag.startsWith("\"")) && (etag.endsWith("\"")))
                 etag = etag.substring(1, etag.length() - 1);
 
             int response = connection.getResponseCode();
@@ -91,7 +91,11 @@ public class Utils {
                 copy(in, out);
             }
 
-            if (etag.indexOf('-') != -1) return true; //No-etag, assume valid
+            if (etag == null) return true; //No-etag, don't store it
+
+            Files.write(etagFile, etag.getBytes());
+
+            if (!"AmazonS3".equals(connection.getHeaderField("server")) || etag.contains("-")) return true; // Etag is not from AmazonS3 which uses plain md5 hashes or the file is a multipart upload in which case the etag is the md5 hash of all parts concatenated after being decoded from hex, assume valid
             String md5 = HashFunction.MD5.hash(file);
 
             if (!etag.equalsIgnoreCase(md5)) {
